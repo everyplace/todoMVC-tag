4a982dbb111f98d214ed87e4679eed9304e09cb9
Greg Skiano <gregoryskiano@gmail.com>
2015-03-25 18:31:24 -0400

    adding example

--- todoMVC-tag.js
+++ todoMVC-tag.js
@@ -1,1 +1,16 @@
-//Hard part goes here
+
+window.addEventListener('WebComponentsReady', function() {
+  var Todo = Object.create(HTMLElement.prototype);
+  
+  Todo.createdCallback = function() {
+    this.innerHTML = "<iframe width='400' height='600' frameBorder='0' src='http://todomvc.com/examples/react/#/'/>";
+  };
+
+  Todo.foo = function() {
+    console.log('foo() called');
+  };
+
+  var XFoo = document.registerElement('todo-mvc', {
+    prototype: Todo
+  });
+});
